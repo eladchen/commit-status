9d99f9eca1dc0203629432303cdb068822d2a9ac
Elad Chen <3628913+eladchen@users.noreply.github.com>
2020-08-27 20:43:48 +0300

    latest

--- build/compiled/index.js
+++ build/compiled/index.js
@@ -18,16 +18,39 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.entryPoint = exports.isPostActionEnvVariable = void 0;
 const core = __importStar(require("@actions/core"));
-const main_1 = require("./actions/main");
-const post_1 = require("./actions/post");
+const action_main_1 = require("./action-main");
+const action_post_1 = require("./action-post");
 const postActionStateIdentifier = "isPost";
-const isPostAction = process.env[`STATE_${postActionStateIdentifier}`] !== undefined;
-if (!isPostAction) {
-    core.saveState(postActionStateIdentifier, "true");
-    main_1.action();
-}
-else {
-    post_1.action();
+const isPostActionEnvVariable = `STATE_${postActionStateIdentifier}`;
+exports.isPostActionEnvVariable = isPostActionEnvVariable;
+const entryPoint = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const isPostAction = process.env[isPostActionEnvVariable] !== undefined;
+        if (!isPostAction) {
+            core.saveState(postActionStateIdentifier, "true");
+            yield action_main_1.action();
+        }
+        else {
+            yield action_post_1.action();
+        }
+    }
+    catch (e) {
+        core.setFailed(e);
+    }
+});
+exports.entryPoint = entryPoint;
+if (require.main === module) {
+    entryPoint();
 }
--- build/compiled/octokit.js
+++ build/compiled/octokit.js
@@ -18,12 +18,36 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.octokit = void 0;
-const core = __importStar(require("@actions/core"));
+exports.listJobsForWorkflowRun = exports.createCommitStatus = exports.getWorkflowRun = void 0;
 const github = __importStar(require("@actions/github"));
-const octokit = () => {
-    const token = core.getInput("token", { required: true });
+const action_inputs_1 = require("./action-inputs");
+const getOctokit = () => {
+    const token = action_inputs_1.tokenInput();
     return github.getOctokit(token);
 };
-exports.octokit = octokit;
+const getWorkflowRun = (parameters) => __awaiter(void 0, void 0, void 0, function* () {
+    const octokit = getOctokit();
+    return yield octokit.actions.getWorkflowRun(parameters);
+});
+exports.getWorkflowRun = getWorkflowRun;
+const createCommitStatus = (params) => __awaiter(void 0, void 0, void 0, function* () {
+    const octokit = getOctokit();
+    return yield octokit.repos.createCommitStatus(params);
+});
+exports.createCommitStatus = createCommitStatus;
+const listJobsForWorkflowRun = (params) => __awaiter(void 0, void 0, void 0, function* () {
+    const octokit = getOctokit();
+    const workflowRunJobs = yield octokit.paginate(octokit.actions.listJobsForWorkflowRun, params);
+    return workflowRunJobs;
+});
+exports.listJobsForWorkflowRun = listJobsForWorkflowRun;
