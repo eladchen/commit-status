5f013d571c3fa738dda5153d58e2eb715165d9c4
Elad Chen <3628913+eladchen@users.noreply.github.com>
2020-08-29 11:08:42 +0300

    ci(workflows): change semantic release config

--- release.config.js
+++ release.config.js
@@ -1,29 +1,28 @@
 // https://semantic-release.gitbook.io/semantic-releas
 // https://github.com/semantic-release/semantic-release/blob/master/docs/usage/configuration.md#configuration
+const plugin = (name, options) => [name, options];
+
 module.exports = {
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-conventionalcommits
-        preset: "conventionalcommits",
-        presetConfig: {},
-      },
-    ],
+    plugin("@semantic-release/commit-analyzer", {
+      // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-conventionalcommits
+      preset: "conventionalcommits",
+      presetConfig: {},
+    }),
+
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
 
     // https://github.com/semantic-release/git
-    [
-      "@semantic-release/git",
-      {
-        assets: ["package.json", "CHANGELOG.md"],
-        // eslint-disable-next-line no-template-curly-in-string
-        message: "chore(release): ${nextRelease.version} [skip-ci]\n\n${nextRelease.notes}",
-      },
-    ],
+    plugin("@semantic-release/git", {
+      assets: ["package.json", "CHANGELOG.md"],
+      // eslint-disable-next-line no-template-curly-in-string
+      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    }),
 
     // https://github.com/semantic-release/github
-    "@semantic-release/github",
+    plugin("@semantic-release/github", {
+      assets: "build",
+    }),
   ],
 };
