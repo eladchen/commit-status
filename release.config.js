18489a596c52231430887090ab9d707045f13992
Elad Chen <3628913+eladchen@users.noreply.github.com>
2020-08-29 11:41:25 +0300

    feat: commit build folder on release

--- release.config.js
+++ release.config.js
@@ -4,8 +4,8 @@ const plugin = (name, options) => [name, options];
 
 module.exports = {
   plugins: [
+    // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-conventionalcommits
     plugin("@semantic-release/commit-analyzer", {
-      // https://github.com/conventional-changelog/conventional-changelog/tree/master/packages/conventional-changelog-conventionalcommits
       preset: "conventionalcommits",
       presetConfig: {},
     }),
@@ -15,14 +15,10 @@ module.exports = {
 
     // https://github.com/semantic-release/git
     plugin("@semantic-release/git", {
-      assets: ["package.json", "CHANGELOG.md"],
-      // eslint-disable-next-line no-template-curly-in-string
-      message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      assets: ["package.json", "CHANGELOG.md", "build"],
     }),
 
     // https://github.com/semantic-release/github
-    plugin("@semantic-release/github", {
-      assets: "build",
-    }),
+    "@semantic-release/github",
   ],
 };
